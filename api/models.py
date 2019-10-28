6ef0dab0b99cb79edbf643a4f01ef89af49fd427
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-10-28 15:08:54 +0300

    Create steps as a model instead of just a textfield

--- api/models.py
+++ api/models.py
@@ -22,6 +22,12 @@ class MealTime(models.Model):
 		return self.name
 
 
+class Step(models.Model):
+	instruction = models.TextField()
+	required_time = models.DurationField()
+	recipe = models.ForeignKey(Recipe, related_name="steps", on_delete=models.CASCADE)
+
+
 class Cuisine(models.Model):
 	name = models.CharField(max_length=100)
 
@@ -33,15 +39,16 @@ class Recipe(models.Model):
 	title = models.CharField(max_length=100)
 	description = models.TextField()
 	image = models.ImageField(blank=True)
-	steps = models.TextField(blank=True)
-	ingredients = models.ManyToManyField(Ingredient, related_name="recipe")
-	meal_type = models.ManyToManyField(MealType, related_name="recipe")
-	meal_time = models.ManyToManyField(MealTime, related_name="recipe")
-	required_time = models.DurationField()
-	cuisine = models.ForeignKey(Cuisine, null=True, related_name="recipe", on_delete=models.SET_NULL)
+	ingredients = models.ManyToManyField(Ingredient, related_name="recipes")
+	meal_type = models.ManyToManyField(MealType, related_name="recipes")
+	meal_time = models.ManyToManyField(MealTime, related_name="recipes")
+	cuisine = models.ForeignKey(Cuisine, null=True, related_name="recipes", on_delete=models.SET_NULL)
 
 	class Meta:
 		verbose_name_plural = "recipies"
 
 	def __str__(self):
 		return self.title
+
+	def get_total_time(self):
+		return sum([time for time in self.steps.required_time])
