65fba58939c8f02a494afb26fcb1c3ddf0694353
AnfalHussain <anfalae95@gmail.com>
2019-10-28 19:44:39 +0300

    CORS headers were added

--- recipickx/settings.py
+++ recipickx/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api',
-
+    'corsheaders',
     'rest_framework',
 ]
 
@@ -59,7 +59,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
+
+
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = 'recipickx.urls'
 
