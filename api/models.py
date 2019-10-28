c6120e7a3db8a3a522562a346796d7a1a8a28134
colkman <h.ali.alhaddad@hotmail.com>
2019-10-28 13:00:17 +0300

    First model structure

--- api/models.py
+++ api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+class MealType(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+class MealTime(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+class Cuisine(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+class Recipe(models.Model):
+	title = models.CharField(max_length=100)
+	description = models.TextField()
+	image = models.ImageField(blank=True)
+	steps = models.TextField(blank=True)
+	ingredients = models.ManyToManyField(Ingredient, related_name="recipe")
+	meal_type = models.ManyToManyField(MealType, related_name="recipe")
+	meal_time = models.ManyToManyField(MealTime, related_name="recipe")
+	required_time = models.DurationField()
+	cuisine = models.ForeignKey(Cuisine, null=True, related_name="recipe", on_delete=models.SET_NULL)
+
+	class Meta:
+		verbose_name_plural = "recipies"
+
+	def __str__(self):
+		return self.title
--- recipickx/settings.py
+++ recipickx/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'api',
 
     'rest_framework',
@@ -139,3 +139,4 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
