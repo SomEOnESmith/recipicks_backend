838ec32b9488dfc8baea184068f3e81e8dc2258d
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-11-06 11:18:28 +0300

    Consolidate search function into one view and add more filters

--- api/views.py
+++ api/views.py
@@ -5,10 +5,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.filters import SearchFilter
+from rest_framework.status import HTTP_200_OK
 
 from .serializers import (
-	UserCreateSerializer, CreateUpdateProfileSerializer, RecipeDetailsSerializer,
-	RecipesListSerializer, IngredientSerializer
+	UserCreateSerializer, CreateUpdateProfileSerializer, RecipeDetailSerializer,
+	RecipeListSerializer, IngredientSerializer
 )
 from .models import Recipe, Profile, Ingredient
 
@@ -25,34 +26,48 @@ class ProfileView(RetrieveUpdateAPIView):
 		return self.request.user.profile
 
 
-class RecipeListView(ListAPIView):
+class RecipeDetailView(RetrieveAPIView):
 	queryset = Recipe.objects.all()
-	serializer_class = RecipesListSerializer
-	filter_backends = [SearchFilter,]
-	search_fields = ['title','meal__name', 'cuisine__name', 'course__name']
-
-
-class RecipeDetailsView(RetrieveAPIView):
-	queryset = Recipe.objects.all()
-	serializer_class = RecipeDetailsSerializer
+	serializer_class = RecipeDetailSerializer
 	lookup_field = 'id'
 	lookup_url_kwarg = 'recipe_id'
 
 
-class IngredientsListView(ListAPIView):
+class IngredientListView(ListAPIView):
 	queryset = Ingredient.objects.all()
 	serializer_class = IngredientSerializer
 
 
-class RecipesByIngredientListView(APIView):
-	def post(self,request):
-		recipes = Recipe.objects.filter(ingredients__id__in=request.data).distinct()
-		exact_match = [recipe for recipe in recipes if set(recipe.ingredients.values_list('id',flat=True))==set(request.data)]
-		user_has_excess = [recipe for recipe in recipes if set(recipe.ingredients.values_list('id',flat=True)).issubset(request.data) and recipe not in exact_match]
-		user_has_missing = list(set(recipes).difference(exact_match, user_has_excess))
+class RecipeListView(APIView):
+	serializer_class = RecipeListSerializer
+	filter_backends = [SearchFilter,]
+	search_fields = ['title','meal__name', 'cuisine__name', 'course__name']
+
+	def search(self, recipes, ingredients):
+		results = {'exact': [], 'excess': [], 'missing': []}
+		for recipe in recipes:
+			ingredients = recipe.ingredients.values_list('id', flat=True)
+			if ingredients ==ingredients:
+				results['exact'].append(recipe)
+			elif set(ingredients).issubset(ingredients):
+				results['excess'].append(recipe)
+			else:
+				results['missing'].append(recipe)
+		return results
+
+	def get(self,request):
+		meal = request.GET.get("meal")
+		cuisine = request.GET.get("cuisine")
+		course = request.GET.get("course")
+		ingredients = request.GET.getlist("ingredients[]")
+		context = {'request': request}
+		if not ingredients:
+			return Response(self.serializer_class(Recipe.objects.all(), context=context, many=True).data)
+		recipes = Recipe.objects.filter(ingredients__id__in=ingredients).distinct()
+		results = self.search(recipes=recipes, ingredients=ingredients)
 		data = {
-			'exact_match': RecipesListSerializer(exact_match, context={'request': request}, many=True).data,
-			'user_has_excess_ingredients': RecipesListSerializer(user_has_excess, context={'request': request}, many=True).data,
-			'user_has_missing_ingredients': RecipesListSerializer(user_has_missing, context={'request': request}, many=True).data
+			'perfect_match': self.serializer_class(results['exact'], context=context, many=True).data,
+			'user_excess_ings': self.serializer_class(results['excess'], context=context, many=True).data,
+			'user_missing_ings': self.serializer_class(results['missing'], context=context, many=True).data
 		}
-		return Response(data, status=200)
+		return Response(data, status=HTTP_200_OK)
