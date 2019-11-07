7e733819a909b83029f8edad4fef9396a3a54435
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-11-07 14:04:31 +0300

    Remove identical URLs

--- api/urls.py
+++ api/urls.py
@@ -13,9 +13,7 @@ urlpatterns = [
     path('recipes/', RecipeListView.as_view(), name='recipes-list'),
     path('recipes/<int:recipe_id>/', RecipeDetailView.as_view(), name='recipe-detail'),
     path('ingredients/', IngredientListView.as_view(), name='ingredients-list'),
-    path('recipes/ingredients/', RecipeListView.as_view(), name='recipes-ingredients'),
     path('cuisines/', CuisineListView.as_view(), name='cuisine-list'),
     path('meals/', MealListView.as_view(), name='meal-list'),
-    path('courses/', CourseListView.as_view(), name='course-list'),
-
+    path('courses/', CourseListView.as_view(), name='course-list')
 ]
