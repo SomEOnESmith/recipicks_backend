2eedd253ca8329b5eb82ad83a26462c3613255a8
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-11-11 14:11:09 +0300

    Add InLine for Images, and customized Recipe admin

--- api/admin.py
+++ api/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
-from .models import (Recipe, Profile, Ingredient, Cuisine, Course, Meal, Step,Ingredient)
+from .models import (Recipe, Profile, Ingredient, Cuisine, Course, Meal, Step,Ingredient, Image)
 
 class StepInline(admin.TabularInline):
 	model = Step
 	extra = 1
 
 
+class ImageInLine(admin.TabularInline):
+	model = Image
+	extra = 1
+
+
 class RecipeAdmin(admin.ModelAdmin):
-	list_display = ('title',)
-	inlines = [
+	list_display = ('id','title','cuisine','total_time')
+	list_display_links = ('id', 'title')
+	inlines = (
 		StepInline,
-	]
+		ImageInLine
+	)
 
 
 admin.site.register(Recipe, RecipeAdmin)
