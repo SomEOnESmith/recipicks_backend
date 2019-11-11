1c9f56806332f980bcff17eb717c9781f221dbc2
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-11-12 02:03:31 +0300

    Admin page customizations

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import (Recipe, Profile, Ingredient, Cuisine, Course, Meal, Step,Ingredient, Image)
+from .models import Course, Cuisine, Ingredient, Image, Meal, Profile, Recipe, Step
+
+
+class IngredientAdmin(admin.ModelAdmin):
+	list_display = ('name', 'category', 'id')
+
+
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user', 'gender', 'phone', 'date_of_birth')
+	readonly_fields = ('name', 'email')
+	list_display_links = ('id', 'user')
+
+	def name(self, obj):
+		return obj.user.get_full_name()
+	
+	def email(self,obj): 
+		return obj.user.email
+
 
 class StepInline(admin.TabularInline):
 	model = Step
@@ -14,16 +31,16 @@ class ImageInLine(admin.TabularInline):
 class RecipeAdmin(admin.ModelAdmin):
 	list_display = ('id','title','cuisine','total_time')
 	list_display_links = ('id', 'title')
+	list_filter = ('cuisine', 'courses', 'meals')
 	inlines = (
 		StepInline,
 		ImageInLine
 	)
 
 
-admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Profile)
-admin.site.register(Ingredient)
-admin.site.register(Cuisine)
 admin.site.register(Course)
+admin.site.register(Cuisine)
+admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Meal)
-admin.site.register(Step)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Recipe, RecipeAdmin)
