70b8f752d8ae447f4bdfaf0ea988ff253adc01ad
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-11-12 02:14:18 +0300

    Alphabetize imports

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .views import (
-	UserCreateAPIView, ProfileView, RecipeListView,
-	RecipeDetailView, FilterView, RecipeCreateAPIView
+	FilterView, ProfileView, RecipeCreateAPIView,
+	RecipeDetailView, RecipeListView, UserCreateAPIView 
 )
 
 urlpatterns = [
 	path('login/', TokenObtainPairView.as_view() , name='login'),
 	path('register/', UserCreateAPIView.as_view(), name='register'),
 	path("profile/", ProfileView.as_view(), name="profile"),
+	path('filters/', FilterView.as_view(), name='filters'),
 	path('recipes/', RecipeListView.as_view(), name='recipes-list'),
 	path('recipes/<int:recipe_id>/', RecipeDetailView.as_view(), name='recipe-detail'),
-	path('filters/', FilterView.as_view(), name='filters'),
 	path('recipe/create/', RecipeCreateAPIView.as_view(), name='recipe-create')
 ]
