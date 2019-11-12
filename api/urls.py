f279320afde41e56a09ce46d4892bbc9df4a3103
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-11-12 12:04:16 +0300

    Testing out pagination

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .views import (
 	FilterView, ProfileView, RecipeCreateAPIView,
-	RecipeDetailView, RecipeListView, UserCreateAPIView 
+	RecipeDetailView, RecipeListView, UserCreateAPIView , RecipeList
 )
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
 	path("profile/", ProfileView.as_view(), name="profile"),
 	path('filters/', FilterView.as_view(), name='filters'),
 	path('recipes/', RecipeListView.as_view(), name='recipes-list'),
+	path('recipes/list/', RecipeList.as_view(), name='recipes-list'),
 	path('recipes/<int:recipe_id>/', RecipeDetailView.as_view(), name='recipe-detail'),
 	path('recipe/create/', RecipeCreateAPIView.as_view(), name='recipe-create')
 ]
